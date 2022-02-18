1a2c9ed59b9d7e758b421e59d2ac0fb44b81dd0c
Elie Gurarie <egurarie@umd.edu>
2022-02-18 16:11:48 -0500

    started the package with some very basic functions

---
+++ R/cols.R
@@ -0,0 +1,45 @@
+#' Break html slide into columns
+#'
+#' Use this in a chunk to create handy html code, but remember to have results = "asis"
+#'
+#' @param side "l" or "r"
+#' @param w width of slide, in percent
+#'
+#' @export
+
+
+cols <- function(side = "l", w = 50){
+    side <- ifelse(side == "l", "left", "right")
+    cat(paste0('<div style="float:', side, '; width: ', w, '%;">'))
+}
+
+#' Insert image
+#'
+#' Use this in a chunk to create handy html code, but remember to have results = "asis"
+#'
+#' @param file relative file location
+#' @param w width of image, in percentage
+#'
+#' @export
+
+pics <- function(file, w=100){
+    cat(paste0("<img src='", file, "' width='",w,"%'/>"))
+}
+
+
+#' Establish Elie-style graphic pars
+#'
+#' I like these:
+#' - mar = c(3,3,2,2),
+#' - bty = "l",
+#' - mgp = c(1.5,.25,0),
+#' - tck = 0.01,
+#' - cex.lab = 1.2
+#'
+#' @param ... additional arguments for par.
+
+
+pars <- function(...){
+    par(mar = c(3,3,2,2), bty = "l", mgp = c(1.5,.25,0),
+        tck = 0.01, cex.lab = 1.2, ...)
+}
